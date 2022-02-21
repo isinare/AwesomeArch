84524fe4269e3e269f49ace7ed0e56c1aa820a9c
isinare <isinare24@hotmail.com>
2022-02-21 22:47:24 +0530

    DotConfigFiles
    Contains Conf Files for the following:
    Alacritty
    Awesome Window Manager
    Better Discord
    Kitty
    Ulauncher
    Picom
    as well as my Wallpapers Folder

---
+++ dotconfig/custom/autostart.sh
@@ -0,0 +1,5 @@
+feh --bg-scale ~/Pictures/Wallpapers/fire-breathing-charizard.jpg
+ulauncher --hide-window
+flameshot
+picom --experimental-backends
+workrave
---
+++ dotconfig/kitty/README.md
@@ -0,0 +1,6 @@
+# kitty-config
+My personal terminal config (again)But this time, **nord**
+
+Place these 2 files in your Kitty config: 
+
+`~/.config/kitty`
