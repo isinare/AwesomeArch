da8a61a660460559302c5717dc1e20322d7ecab3
isinare <91046353+isinare@users.noreply.github.com>
2022-11-07 16:49:55 +0000

    Delete dotconfig directory

--- dotconfig/custom/autostart.sh
+++
@@ -1,5 +0,0 @@
-feh --bg-scale ~/Pictures/Wallpapers/fire-breathing-charizard.jpg
-ulauncher --hide-window
-flameshot
-picom --experimental-backends
-workrave
--- dotconfig/kitty/README.md
+++
@@ -1,6 +0,0 @@
-# kitty-config
-My personal terminal config (again)But this time, **nord**
-
-Place these 2 files in your Kitty config: 
-
-`~/.config/kitty`
